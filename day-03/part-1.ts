d4b82fe955e034cf35b746d92c928019706d5a44
John Pangalos <john@pangalos.dev>
2024-12-03 09:01:41 +0100

    day 03 part 1

--- day-03/part-1.ts
+++ day-03/part-1.ts
@@ -1,5 +1,8 @@
 import * as fs from "fs/promises";
 
 const input = await fs.readFile("./day-03/test.txt", "utf8");
-const temp = [...input.matchAll(/mul\(\d+,\d+\)/)];
-console.log(temp);
+console.log(
+  input
+    .matchAll(/mul\((\d+),(\d+\))/g)
+    .reduce((acc, x) => acc + parseInt(x[1]) * parseInt(x[2]), 0),
+);
