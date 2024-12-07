fc044a7673ca33ff1ee5d87f240560e4d83ae4f6
John Pangalos <john@pangalos.dev>
2024-12-07 21:05:26 +0100

    day 06 - the temporary set back

---
+++ day-06/part-01.ts
@@ -0,0 +1,69 @@
+import { Dirent } from "fs";
+import * as fs from "fs/promises";
+
+type Point = { x: number; y: number; value: string };
+type Direction = "up" | "down" | "left" | "right";
+
+const map = new Map<`${string}-${string}`, Point>();
+
+const guardChar = "^";
+
+let direction: Direction = "up";
+
+const turnMap: Record<Direction, Direction> = {
+  up: "right",
+  right: "down",
+  down: "left",
+  left: "up",
+};
+
+function nextPoint(point: Point, direction: Direction): Point | undefined {
+  switch (direction) {
+    case "up":
+      return map.get(`${point.x}-${point.y - 1}`);
+    case "down":
+      return map.get(`${point.x}-${point.y + 1}`);
+    case "left":
+      return map.get(`${point.x - 1}-${point.y}`);
+    case "right":
+      return map.get(`${point.x + 1}-${point.y}`);
+  }
+}
+
+const input = await fs.readFile("./day-06/input.txt", "utf8");
+const lines = input.split("\n").filter(Boolean);
+lines.forEach((line, y) => {
+  line.split("").forEach((char, x) => {
+    map.set(`${x}-${y}`, { x, y, value: char });
+  });
+});
+let guardRes = [...map.entries()].find(([_, { value }]) => value === guardChar);
+if (!guardRes) throw new Error("No guard found");
+let [_, guard] = guardRes;
+
+while (true) {
+  const checkPoint = nextPoint(guard, direction);
+  if (!checkPoint) {
+    map.set(`${guard.x}-${guard.y}`, { x: guard.x, y: guard.y, value: "X" });
+    break;
+  }
+  if (checkPoint.value === "#") direction = turnMap[direction];
+
+  const next = nextPoint(guard, direction);
+  if (!next) throw new Error("No next point");
+  map.set(`${guard.x}-${guard.y}`, { x: guard.x, y: guard.y, value: "X" });
+  guard = {
+    x: next.x,
+    y: next.y,
+    value: guardChar,
+  };
+  map.set(`${next.x}-${next.y}`, guard);
+}
+
+let count = 0;
+map.forEach(({ value }) => {
+  if (value !== "X") return;
+  count++;
+});
+
+console.log(count);
---
+++ day-06/part-02.ts
@@ -0,0 +1,111 @@
+import { Dirent } from "fs";
+import * as fs from "fs/promises";
+
+type Point = { x: number; y: number; value: string };
+type Direction = "up" | "down" | "left" | "right";
+type Step = { x: number; y: number; value: string; direction: Direction };
+
+let map = new Map<`${string}-${string}`, Point>();
+
+const guardChar = "^";
+const obstacleChar = "#";
+
+let direction: Direction = "up";
+const input = await fs.readFile("./day-06/input.txt", "utf8");
+
+const lines = input.split("\n").filter(Boolean);
+lines.forEach((line, y) => {
+  line.split("").forEach((char, x) => {
+    map.set(`${x}-${y}`, { x, y, value: char });
+  });
+});
+
+const originalMap = new Map(map);
+
+const turnMap: Record<Direction, Direction> = {
+  up: "right",
+  right: "down",
+  down: "left",
+  left: "up",
+};
+
+function nextPoint(
+  point: Point,
+  direction: Direction,
+  map: Map<string, Point>,
+): Point | undefined {
+  switch (direction) {
+    case "up":
+      return map.get(`${point.x}-${point.y - 1}`);
+    case "down":
+      return map.get(`${point.x}-${point.y + 1}`);
+    case "left":
+      return map.get(`${point.x - 1}-${point.y}`);
+    case "right":
+      return map.get(`${point.x + 1}-${point.y}`);
+  }
+}
+
+let guardRes = [...map.entries()].find(([_, { value }]) => value === guardChar);
+if (!guardRes) throw new Error("No guard found");
+let [_, guard] = guardRes;
+const originalGuard = { ...guard };
+
+let points: Set<string> = new Set();
+
+while (true) {
+  const checkPoint = nextPoint(guard, direction, map);
+  if (!checkPoint) {
+    map.set(`${guard.x}-${guard.y}`, { x: guard.x, y: guard.y, value: "X" });
+    break;
+  }
+  if (checkPoint.value === "#") {
+    direction = turnMap[direction];
+    continue;
+  }
+
+  map.set(`${guard.x}-${guard.y}`, { x: guard.x, y: guard.y, value: "X" });
+  guard = {
+    x: checkPoint.x,
+    y: checkPoint.y,
+    value: guardChar,
+  };
+  map.set(`${checkPoint.x}-${checkPoint.y}`, guard);
+}
+
+map.forEach((point) => {
+  if (point.value !== "X") return;
+
+  if (point.x === originalGuard.x && point.y === originalGuard.y) return;
+  let guard = { ...originalGuard };
+  let direction: Direction = "up";
+  const runMap = new Map([...originalMap]);
+  runMap.set(`${point.x}-${point.y}`, { x: point.x, y: point.y, value: "#" });
+  const steps: Set<string> = new Set();
+
+  while (true) {
+    const next = nextPoint(guard, direction, runMap);
+    if (!next) break;
+
+    if (next.value === obstacleChar) {
+      direction = turnMap[direction];
+      continue;
+    }
+
+    guard = {
+      x: next.x,
+      y: next.y,
+      value: guardChar,
+    };
+
+    const step = `${guard.x}-${guard.y}-${direction}`;
+
+    if (steps.has(step)) {
+      points.add(`${point.x}-${point.y}`);
+      break;
+    }
+    steps.add(step);
+  }
+});
+
+console.log(points.size);
